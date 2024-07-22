72d9f4cd93046d724257f0e9c9ba01c329646c54
KayalvizhiV <kayalvizhi3311@gmail.com>
2024-07-22 22:36:39 +0530

    Added get,put,patch,delete actions to organization view

--- myapp/urls/organization_url.py
+++ myapp/urls/organization_url.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from ..views.organization_view import create_org,get_org
+from ..views.organization_view import create_org,get_org, get_org_by_name,get_update_delete_by_id
 
 urlpatterns = [
   # path('user/', OrganizationView.as_view()),
   # path('organization/<str:org_name>/<str:org_short_name>/', OrganizationView.as_view()),
-  path('insertorg', create_org,name="create_org"),
-  path('getorg',get_org,name="get_org")
+  path('insertorg', create_org, name="create_org"),
+  path('getorg', get_org, name="get_org"),
+  path('getorgbyname/<str:name>', get_org_by_name, name = "get_org_by_name"),
+  path('getupdatedeletebyid/<int:id>', get_update_delete_by_id, name = "get_update_delete_by_id")
 ]
--- myapp/views/organization_view.py
+++ myapp/views/organization_view.py
@@ -5,7 +5,6 @@ from ..models.organization_model import OrganizationModel
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-# class OrganizationView(APIView):
 @api_view(["POST"])
 def create_org(request):
     if request.method == "POST":
@@ -13,7 +12,7 @@ def create_org(request):
         if serializer_obj.is_valid():
             serializer_obj.save()
             return Response(serializer_obj.data, status = status.HTTP_201_CREATED )
-        # return Response(serializer_obj.data, status = status.HTTP_400_BAD_REQUEST )
+        return Response(serializer_obj.data, status = status.HTTP_400_BAD_REQUEST )
         
 
 @api_view(["GET"])
@@ -21,10 +20,42 @@ def get_org(request):
     if request.method == "GET":
         org_model = OrganizationModel.objects.all()
         serializer = OrganizationSerializer(org_model, many=True)
-        print(serializer.data)
         return Response(serializer.data)
-        # return Response(serializer_obj.data, status = status.HTTP_400_BAD_REQUEST )
         
+@api_view(["GET"])
+def get_org_by_name(request, name):
+    if request.method == "GET":
+        try:
+            org_model = OrganizationModel.objects.filter(org_name=name)
+            serializer = OrganizationSerializer(org_model, many=True)
+            return Response(serializer.data, status = 200)
+        except OrganizationModel.DoesNotExist:
+            return Response({'success': False,'message': f"ORG_NAME {name} doesn't exist."}, status=400)
 
+@api_view(['GET','PUT','DELETE','PATCH'])
+def get_update_delete_by_id(request,id):
+    try:
+        queryset = OrganizationModel.objects.get(org_id = id)
+        if request.method == "GET":
+            serializer = OrganizationSerializer(queryset)
+            return Response(serializer.data, status = 200)
 
+        if request.method == 'PUT':
+            serializer = OrganizationSerializer(queryset, data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'result': serializer.data, 'message': 'Updated successfully'}, status = 204)
+
+        if request.method == 'PATCH':
+            serializer = OrganizationSerializer(queryset, data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'result': serializer.data, 'message': 'Updated/Patch successfully'}, status = 204)
+
+        if request.method == 'DELETE':
+            queryset.delete()
+            return Response({'message': 'Deleted successfully'}, status = 204)
+        
+    except OrganizationModel.DoesNotExist:
+            return Response({'success': False,'message': f"ORG_ID {id} doesn't exist."}, status=400)
 
